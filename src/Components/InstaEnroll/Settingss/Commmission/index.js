1445b7710856d81dd3a35334ab0d00ca3ab78480
jubairskv <jubairskv@gmail.com>
2025-03-05 18:01:46 +0530

    Auth page navigation issue in commission updated

--- src/Components/InstaEnroll/Settingss/Commmission/index.js
+++ src/Components/InstaEnroll/Settingss/Commmission/index.js
@@ -2,6 +2,6 @@ export { default as Commission } from "./Commission";
 export { default as AddCommission } from "./AddCommission";
 export { default as EditCommission } from "./EditCommission";
 export { default as ViewCommission } from "./ViewCommission";
-export { default as AuthCommission } from "./EditCommission";
+export { default as AuthCommission } from "./AuthCommission";
 export { default as DeleteCommission } from "./DeleteCommission";
 export { default as CommissionForm } from "./CommissionForm";
