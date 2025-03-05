f2c652dd221dda8905ac7c06d4a3d0e31a9a42dc
jubairskv <jubairskv@gmail.com>
2025-03-05 11:09:02 +0530

    Fuel ManagementFolder addded

--- src/Components/InstaEnroll/Settingss/ProofOfIdentity/index.js
+++ src/Components/InstaEnroll/Settingss/ProofOfIdentity/index.js
@@ -2,5 +2,5 @@ export { default as ProofOfIdentity } from "./ProofOfIdentity";
 export { default as AddProofOfIdentity } from "./AddProofOfIdentity";
 export { default as EditProofOfIdentity } from "./EditProofOfIdentity";
 export { default as ViewProofOfIdentity } from "./ViewProofOfIdentity";
-export { default as AuthProofOfIdentity } from "./AuthProofOfIdentity";
+export { default as AuthProofOfIdentity } from "./AuthProofofIdentity";
 export { default as DeleteProofOfIdentity } from "./DeleteProofOfIdentity";
